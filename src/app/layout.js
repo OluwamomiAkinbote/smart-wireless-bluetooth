a19f278dec93fb3e0c5c65ba9a63067fadec9b2a
oluwamomiAkinbote <oluwamomis@gmail.com>
2025-09-10 15:46:51 +0100

    using built in google fxn

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import FloatingContactWidget from '@/components/FloatingContactWidget'
-import Script from 'next/script'
+// Correct import - this module is built into your Next.js version
+import { Facebook } from 'next/third-parties/google'
 
 export const metadata = {
   title: {
@@ -44,32 +45,12 @@ export default function RootLayout({ children }) {
         <FloatingContactWidget />
         {children}
 
-        {/* ✅ Facebook Pixel Script */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1176033614217437');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        {/* ✅ CORRECT: Using the built-in Facebook Pixel component */}
+        <Facebook
+          appId=""
+          pixelId="1176033614217437" // Ensure this is your correct Pixel ID
+        />
 
-        {/* ✅ NoScript Fallback */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1176033614217437&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
       </body>
     </html>
   )
